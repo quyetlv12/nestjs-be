7940434758297e4dceb77ada114bfe8c93199c3f
NGUYEN NGOC HUNG <davisdavis448@gmail.com>
2025-06-23 21:13:05 +0700

    fix: create talent permission, return data talent

--- src/modules/talents/talents.service.ts
+++ src/modules/talents/talents.service.ts
@@ -148,14 +148,14 @@ export class TalentsService {
   findOne(id: number) {
     return this.talentRepository.findOne({
       where: { id },
-      relations: ['videos'],
+      relations: ['videos', "categories", 'roles'],
     });
   }
 
   findByEmail(email: string) {
     return this.talentRepository.findOne({
       where: { email },
-      relations: ['roles'],
+      relations: ['roles', 'categories', 'videos'],
       select: {
         id: true,
         name: true,
