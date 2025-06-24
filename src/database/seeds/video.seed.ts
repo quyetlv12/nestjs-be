5aa58226fa786f9e58045069a83a3a419811d1a6
quyetlv12 <lequyet840500@gmail.com>
2025-06-24 14:42:48 +0700

    fixbug register status

--- src/database/seeds/video.seed.ts
+++ src/database/seeds/video.seed.ts
@@ -7,14 +7,6 @@ export const seedVideos = async (dataSource: DataSource) => {
   const videoRepo = dataSource.getRepository(Video);
   const userRepo = dataSource.getRepository(User);
   const categoryRepo = dataSource.getRepository(Category);
-
-  // Lấy talent user
-  const talent = await userRepo.findOne({ where: { email: 'talent@example.com' } });
-  if (!talent) {
-    console.warn('⚠️ Talent user not found');
-    return;
-  }
-
   // Lấy category
   const category = await categoryRepo.findOne({ where: { id: 1 } });
   if (!category) {
@@ -22,25 +14,99 @@ export const seedVideos = async (dataSource: DataSource) => {
     return;
   }
 
-  const videos = [
-    {
-      title: 'Talent Music Video',
-      videoLink: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
-      duration: '3:30',
-      createdBy: talent,
-      createdById: talent.id,
-      categoryId: category.id, // Nếu có quan hệ category-video
-      thumbnailLink: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
-      price: 100000,
-      hide_video: false,
-    },
-  ];
+  const talents = [7, 8, 9, 10];
 
-  for (const v of videos) {
-    const exists = await videoRepo.findOneBy({ title: v.title });
-    if (!exists) {
-      await videoRepo.save(videoRepo.create(v));
-      console.log(`✅ Seeded video: ${v.title}`);
+  for (const talentId of talents) {
+    const talent = await userRepo.findOne({ where: { id: talentId } });
+    console.log("talent" , talent);
+    
+    if (!talent) {
+      console.warn('⚠️ Talent not found');
+      continue;
+    }
+
+    const videos = [
+      {
+        title: 'Talent Music Video',
+        videoLink: 'https://cdn.pixabay.com/video/2025/03/23/266987_large.mp4',
+        duration: '3:30',
+        createdBy: talent,
+        createdById: talent.id,
+        categoryId: category.id, // Nếu có quan hệ category-video
+        thumbnailLink:
+          'https://cdn.pixabay.com/photo/2025/06/19/16/21/adventure-9669330_1280.jpg',
+        price: 100000,
+        hide_video: false,
+      },
+      {
+        title: 'Talent Music Videortyrtytr',
+        videoLink: 'https://cdn.pixabay.com/video/2025/03/23/266987_large.mp4',
+        duration: '3:30',
+        createdBy: talent,
+        createdById: talent.id,
+        categoryId: category.id, // Nếu có quan hệ category-video
+        thumbnailLink:
+          'https://cdn.pixabay.com/photo/2025/06/19/16/21/adventure-9669330_1280.jpg',
+        price: 100000,
+        hide_video: false,
+      },
+      {
+        title: 'Talent Music Videotỷyrt',
+        videoLink: 'https://cdn.pixabay.com/video/2025/03/23/266987_large.mp4',
+        duration: '3:30',
+        createdBy: talent,
+        createdById: talent.id,
+        categoryId: category.id, // Nếu có quan hệ category-video
+        thumbnailLink:
+          'https://cdn.pixabay.com/photo/2025/06/19/16/21/adventure-9669330_1280.jpg',
+        price: 100000,
+        hide_video: false,
+      },
+
+      {
+        title: 'Talent Music Video',
+        videoLink: 'https://cdn.pixabay.com/video/2025/03/23/266987_large.mp4',
+        duration: '3:30',
+        createdBy: talent,
+        createdById: talent.id,
+        categoryId: category.id, // Nếu có quan hệ category-video
+        thumbnailLink:
+          'https://cdn.pixabay.com/photo/2025/06/19/16/21/adventure-9669330_1280.jpg',
+        price: 100000,
+        hide_video: false,
+      },
+      {
+        title: 'Talent Music Videortyrtytr',
+        videoLink: 'https://cdn.pixabay.com/video/2025/03/23/266987_large.mp4',
+        duration: '3:30',
+        createdBy: talent,
+        createdById: talent.id,
+        categoryId: category.id, // Nếu có quan hệ category-video
+        thumbnailLink:
+          'https://cdn.pixabay.com/photo/2025/06/19/16/21/adventure-9669330_1280.jpg',
+        price: 100000,
+        hide_video: false,
+      },
+      {
+        title: 'Talent Music Videotỷyrt',
+        videoLink: 'https://cdn.pixabay.com/video/2025/03/23/266987_large.mp4',
+        duration: '3:30',
+        createdBy: talent,
+        createdById: talent.id,
+        categoryId: category.id, // Nếu có quan hệ category-video
+        thumbnailLink:
+          'https://cdn.pixabay.com/photo/2025/06/19/16/21/adventure-9669330_1280.jpg',
+        price: 100000,
+        hide_video: false,
+      },
+    ];
+
+    for (const v of videos) {
+      const exists = await videoRepo.findOneBy({ title: v.title });
+      if (!exists) {
+        await videoRepo.save(videoRepo.create(v));
+        console.log(`✅ Seeded video: ${v.title}`);
+      }
     }
   }
-}; 
+};
