182b4b24dbf035e63b3df70954b33afe3eb628bb
quyetlv12 <lequyet840500@gmail.com>
2025-07-03 10:35:41 +0700

    update build

--- src/database/typeorm.config.ts
+++ src/database/typeorm.config.ts
@@ -2,29 +2,29 @@
 import { dbConfig } from 'src/config/dbConfig';
 import { DataSource, DataSourceOptions } from 'typeorm';
 
-export const dataSourceOptions: DataSourceOptions = {
-  type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: dbConfig.username,
-  password: dbConfig.password,
-  database: dbConfig.database,
-  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-  migrations: [__dirname + '/migrations/*{.ts,.js}'],
-  synchronize: false,
-};
-
-
 // export const dataSourceOptions: DataSourceOptions = {
 //   type: 'postgres',
-//   url: 'postgresql://cammeo_owner:npg_Rfy2LJaYeT6H@ep-sparkling-paper-a8kk0u2d-pooler.eastus2.azure.neon.tech/cammeo?sslmode=require',
-//   ssl: {
-//     rejectUnauthorized: false, // Neon yêu cầu SSL
-//   },
-//   synchronize: true, // ❗Chỉ dùng khi dev
+//   host: 'localhost',
+//   port: 5432,
+//   username: dbConfig.username,
+//   password: dbConfig.password,
+//   database: dbConfig.database,
 //   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
 //   migrations: [__dirname + '/migrations/*{.ts,.js}'],
+//   synchronize: false,
 // };
+
+
+export const dataSourceOptions: DataSourceOptions = {
+  type: 'postgres',
+  url: 'postgresql://cammeo_owner:npg_Rfy2LJaYeT6H@ep-sparkling-paper-a8kk0u2d-pooler.eastus2.azure.neon.tech/cammeo?sslmode=require',
+  ssl: {
+    rejectUnauthorized: false, // Neon yêu cầu SSL
+  },
+  synchronize: false, // ❗Chỉ dùng khi dev
+  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+  migrations: [__dirname + '/migrations/*{.ts,.js}'],
+};
 
 
 
